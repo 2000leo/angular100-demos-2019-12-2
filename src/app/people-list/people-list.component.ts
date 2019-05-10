67d0186dcf860a83dbe15bf8ca39ae8a1d90e143
cioffi42 <cioffi42@gmail.com>
2019-05-10 17:59:54 -0400

    delete package-lock.json

---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-list',
+  template: `
+  <div class="container">
+  <div class="card-deck">
+    <app-person [name]="peopleArray[0]"
+    (personClicked)="parentFunctionHandler($event)"></app-person>
+    <app-person [name]="peopleArray[1]"
+    (personClicked)="parentFunctionHandler($event)"></app-person>
+    <app-person [name]="peopleArray[2]"
+    (personClicked)="parentFunctionHandler($event)"></app-person>
+  </div>
+  </div>
+    `
+})
+export class PeopleListComponent implements OnInit {
+  peopleArray: any[];
+
+  ngOnInit() {
+    this.peopleArray = ['April', 'May', 'June'];
+  }
+
+  parentFunctionHandler(name) {
+      alert(`You sent ${name} up to list from child`);
+  }
+
+}
