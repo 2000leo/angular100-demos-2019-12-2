a0386f442fe5e69855ed20e763174585455ef0aa
cioffi42 <cioffi42@gmail.com>
2019-05-10 18:00:40 -0400

    delete package-lock.json

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { PersonComponent } from './person/person.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeopleListComponent,
+    PersonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
