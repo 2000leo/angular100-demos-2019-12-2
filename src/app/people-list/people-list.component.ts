ceb0723a6118f4181230fa5e3bcbf6c143b287b1
cioffi42 <cioffi42@gmail.com>
2019-05-10 18:01:23 -0400

    delete package-lock.json

---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.css']
+})
+export class PeopleListComponent  {
+
+}
+
