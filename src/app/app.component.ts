a6fc790dee2a0a133f20a34dbf441fb85d35d394
Judy Lipinki <judy@karmoxie.com>
2019-07-20 21:58:25 -0400

    updated to 8.1.2

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+
+import { PeopleService } from "./shared/people.service";
+import { Person } from "./shared/models/person.model";
+import { Observable } from "rxjs";
+
+@Component({
+  selector: "app-root",
+  template: `
+    <ul>
+      <li *ngFor="let person of (people$ | async)">
+        {{ person.first_name }}
+      </li>
+    </ul>
+  `
+})
+export class AppComponent implements OnInit {
+  people$: Observable<Person[]>;
+
+  constructor(private peopleService: PeopleService) {}
+
+  ngOnInit(): void {
+    this.people$ = this.peopleService.getPeople();
+  }
+}
