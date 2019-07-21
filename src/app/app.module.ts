226295eb46c24daf1ac34cdd72337463d6fa39df
Judy Lipinki <judy@karmoxie.com>
2019-07-21 11:25:53 -0400

    updated to 8.1.2

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PeopleListComponent } from './people/people-list.component';
+import { PersonComponent } from './data/person.component';
+import { ProductListComponent } from './products/product-list.component';
+import { UserListComponent } from './users/user-list.component';
+import { UserCardComponent } from './users/user-card.component';
+
+@NgModule({
+  declarations: [AppComponent, PeopleListComponent, PersonComponent, ProductListComponent, UserListComponent, UserCardComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
